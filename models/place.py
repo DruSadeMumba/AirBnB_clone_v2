36ac76361a2ce772617b8fc5160b9d1bb45fa99c
Dru Mumba <drusademumba@gmail.com>
2023-11-20 15:20:21 +0300

    Update Place: (models/place.py)

--- models/place.py
+++ models/place.py
@@ -9,11 +9,12 @@ from models.base_model import BaseModel, Base
 from models.review import Review
 
 metadata = Base.metadata
-place_amenity = Table("place_amenity", metadata,
-                      Column("place_id", String(60), ForeignKey("places.id"),
-                             primary_key=True, nullable=False),
-                      Column("amenity_id", String(60), ForeignKey(
-                          "amenities.id"), primary_key=True, nullable=False))
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    place_amenity = Table("place_amenity", metadata,
+                          Column("place_id", String(60), ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60), ForeignKey(
+                              "amenities.id"), primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
