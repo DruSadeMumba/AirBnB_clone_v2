c4ddf90799ea2198090590d83fd0b684f7172ed9
Dru Mumba <drusademumba@gmail.com>
2023-11-24 10:15:32 +0300

    correction

--- models/place.py
+++ models/place.py
@@ -33,7 +33,7 @@ class Place(BaseModel, Base):
         reviews = relationship("Review", cascade="all, delete-orphan",
                                backref="place")
         amenities = relationship("Amenity", secondary=place_amenity,
-                                 viewonly=False, backref="places",
+                                 viewonly=False,
                                  back_populates="place_amenities")
     else:
         @property
