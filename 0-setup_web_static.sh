7286e4466bb97df56868ec0f868a47a506a9b974
Dru Mumba <drusademumba@gmail.com>
2023-12-07 12:42:22 +0300

    Prep web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+# Prepare your web servers
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+echo "Testing..." | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
+sudo service nginx start
