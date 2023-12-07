7a03c38ff1e4822f82f71bdfca18b89353a1499c
Dru Mumba <drusademumba@gmail.com>
2023-12-07 13:40:07 +0300

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""Deploy archive"""
+import tarfile
+from os.path import exists
+from fabric.api import env, put, run
+
+env.hosts = ['52.204.67.182', '18.209.180.1']
+
+
+def do_deploy(archive_path):
+    """Deploy archive"""
+    if not exists(archive_path):
+        return False
+
+    try:
+        put(archive_path, '/tmp/')
+        file_name = archive_path.split('/')[-1]
+        folder = f'/data/web_static/releases/{file_name.split(".")[0]}'
+
+        run(f"mkdir -p {folder}")
+        run(f"tar -xzf /tmp/{file_name} -C {folder}")
+
+        run(f"rm -rf /tmp/{file_name}")
+        run(f"mv {folder}/web_static/* {folder}/")
+        run(f"rm -rf {folder}/web_static")
+        run("rm -rf /data/web_static/current")
+
+        run(f"ln -s {folder} /data/web_static/current")
+        return True
+
+    except Exception:
+        return False
