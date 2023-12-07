8fdef87b69a5bbd62f7db23e4dabca082ea9a5d7
Dru Mumba <drusademumba@gmail.com>
2023-12-07 13:03:57 +0300

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Compress before sending"""
+from datetime import datetime
+from os.path import exists
+from fabric.api import local
+import tarfile
+
+
+def do_pack():
+    if not exists("versions"):
+        local("mkdir -p versions")
+
+    t_time = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
+    archive_name = f"versions/web_static_{t_time}.tgz"
+    try:
+        local(f"tar -cvzf {archive_name} web_static")
+        return archive_name
+    except tarfile.TarError:
+        return None
