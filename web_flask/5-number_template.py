2a816e0b691450f626867e66da814b421b982c88
Dru Mumba <drusademumba@gmail.com>
2023-12-21 11:51:33 +0300

    Write a script that starts a Flask web application

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,1 +1,48 @@
 #!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display message: Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display message: HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Display message: C <text>"""
+    msg = text.replace('_', ' ')
+    return f"C {msg}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python(text='is cool'):
+    """Display message: Python <text>"""
+    msg = text.replace('_', ' ')
+    return f"Python {msg}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def numb(n):
+    """Display number"""
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numb_temp(n):
+    """Display number"""
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
