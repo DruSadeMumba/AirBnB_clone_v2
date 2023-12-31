2679c28da16b7e0428fd3ae8360bac8d5f268e2f
Dru Mumba <drusademumba@gmail.com>
2023-12-31 17:53:26 +0300

    Write a script that starts a Flask web application

--- models/state.py
+++ models/state.py
@@ -6,28 +6,19 @@ from sqlalchemy.orm import relationship
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
-import shlex
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete-orphan",
-                          backref="states")
 
-    @property
-    def cities(self):
-        """get cities from file storage"""
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if city[0] == 'City':
-                lista.append(var[key])
-        for elem in lista:
-            if elem.state_id == self.id:
-                result.append(elem)
-        return (result)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", cascade="all, delete-orphan",
+                              backref="states")
+    else:
+        @property
+        def cities(self):
+            """get cities from file storage"""
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
